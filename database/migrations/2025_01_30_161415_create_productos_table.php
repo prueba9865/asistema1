f60e37be83b3ba6fc98fee26664bba45b59eca91
prueba9865 <albertoucendo88@hotmail.com>
2025-01-30 17:59:23 +0100

    añadimos nueva migracion de la tabla "productos" y un seeder de la tabla "users" (aunque este seeder no lo usare ya que prefiero añadir los datos de prueba manualmente al ser tan simple el proyecto)

---
+++ database/migrations/2025_01_30_161415_create_productos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo', 20);
+            $table->text('descripcion');
+            $table->decimal('precio', 10, 2);
+            $table->integer('existencia');
+            $table->tinyInteger('activo');           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
